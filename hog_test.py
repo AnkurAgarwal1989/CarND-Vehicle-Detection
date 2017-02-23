b827fe08d22719e6d009a909cbcae50633b52d2a
Ankur Agarwal <ankuraga@gmail.com>
2017-02-22 16:22:10 -0800

    Create hog_test.py
    File to test functionality of HOG module

---
+++ hog_test.py
@@ -0,0 +1,41 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+from hog_features import hog_features
+
+
+# Read in our vehicles and non-vehicles
+images = glob.glob('*.jpeg')
+cars = []
+notcars = []
+
+for image in images:
+    if 'image' in image or 'extra' in image:
+        notcars.append(image)
+    else:
+        cars.append(image)
+
+# Generate a random index to look at a car image
+ind = np.random.randint(0, len(cars))
+# Read in the image
+image = mpimg.imread(cars[ind])
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# Define HOG parameters
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+# Call our function with vis=True to see an image output
+features, hog_image = hog_features(gray, orient, 
+                        pix_per_cell, cell_per_block, 
+                        vis=True, feature_vec=False)
+
+# Plot the examples
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(image, cmap='gray')
+plt.title('Example Car Image')
+plt.subplot(122)
+plt.imshow(hog_image, cmap='gray')
+plt.title('HOG Visualization')
